65004e4f091501694fb6061d8dc04fd7fa43b271
holerbah <holerbah@gmail.com>
2024-04-15 23:25:38 +0200

    proba

--- main.go
+++ main.go
@@ -7,17 +7,24 @@ import (
 )
 
 func main() {
-	repo := repository.NewConfigInMemRepository();
-	service := service.NewConfigService(repo);
+	repo := repository.NewConfigInMemRepository()
+	service := service.NewConfigService(repo)
 	params := make(map[string]string)
 	params["username"] = "pera"
 	params["port"] = "5432"
 	config := model.Config{
-		Name:    "viktorova",
-		Version: 2,
-		Parameters:  params,
+		Name:       "viktorova",
+		Version:    2,
+		Parameters: params,
 	}
-	
+	config2 := model.Config{
+		Name:       "viktorova",
+		Version:    2,
+		Parameters: params,
+	}
 	service.AddConfig(config)
 	service.GetConfig(config.Name, config.Version)
+	service.AddConfig(config2)
+	service.GetConfig(config2.Name, config2.Version)
+
 }
